c5774cf4682332309f9f186b4a80c7d26f5d408b
Ziyan Junaideen <jdeen-solutions@outlook.com>
2014-08-17 22:47:59 +0530

    User type

--- src/models/general/user.rb
+++ src/models/general/user.rb
@@ -12,9 +12,10 @@ class User
   property :created_at,       DateTime
   property :updated_at,       DateTime
 
-  property :email,            String,       length: 50, unique: true
+  property :type,             Enum[:user, :admin],      default: :user
+  property :email,            String,                   length: 50, unique: true
   property :password,         BCryptHash
-  property :display_name,     String,       length: 25
+  property :display_name,     String,                   length: 25
 
   def authentic? password
     self.password == password
